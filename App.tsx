7fed050a3c5a93ab268eeee40daedb486893af63
kev <kev.hannen@gmail.com>
2024-07-25 21:31:53 +0100

    more initial commits

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import {hello, View1, View2} from './out/Library'
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
+// noinspection JSUnusedGlobalSymbols
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={View1} />
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ src/Library.fs
@@ -0,0 +1,23 @@
+﻿namespace App
+open Feliz.ReactNative
+
+module Say =
+    let hello name =
+         $"Hello {name},"
+         
+    let private middleCentre = [style.flex 1; style.alignItems.center; style.justifyContent.center]
+    [<ReactComponent>]     
+    let View1 () =
+        Comp.view [
+            prop.style middleCentre
+            prop.children [
+                Comp.text [ prop.text "View1" ]
+                Comp.statusBar []
+            ]
+        ]
+        
+    let View2 () =
+        Comp.view [
+            Comp.text [ prop.text "View2" ]
+            Comp.statusBar []
+        ]
